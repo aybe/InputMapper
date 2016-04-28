c10c8d4456d51b06361e0b925766d770a4570cf8
Aybe <aybe@users.noreply.github.com>
2016-04-28 03:05:09 +0200

    Update IDeviceButton.cs

--- Assets/InputMapper/IDeviceButton.cs
+++ Assets/InputMapper/IDeviceButton.cs
@@ -12,14 +12,9 @@ namespace Assets.InputMapper
         string GetDescription();
 
         /// <summary>
-        ///     Gets analog value for this button (see Remarks).
+        ///     Gets analog value for this button.
         /// </summary>
         /// <returns>A value between 0.0 and 1.0.</returns>
-        /// <remarks>
-        ///     When underlying button is an emulation such as an analog keyboard button, some easing function such as 'back'
-        ///     will return a value outside the 0.0 to 1.0 range. Care should be taken if such easing is used within an axis
-        ///     command, i.e. it will behave as if the opposite button of the axis command was pressed instead.
-        /// </remarks>
         float GetValue();
 
         /// <summary>
